0e89e304c358a387fd84742e8888501842da01fd
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-12-13 00:39:10 +0800

    bigimg fancybox work

--- lib/render.py
+++ lib/render.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 # File: render.py
-# Date: Fri Dec 12 23:15:31 2014 +0800
+# Date: Sat Dec 13 00:29:29 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 import os
 import base64
-import eyed3
+import glob
 import logging
 logger = logging.getLogger(__name__)
+
+import eyed3
 LIB_PATH = os.path.dirname(os.path.abspath(__file__))
-CSS_FILE = os.path.join(LIB_PATH, 'static/wx.css')
 HTML_FILE = os.path.join(LIB_PATH, 'static/template.html')
 
 try:
@@ -30,10 +31,28 @@ TEMPLATES = dict([(k, open(os.path.join(
 
 class HTMLRender(object):
     def __init__(self, parser, res=None):
-        self.css = ensure_unicode(css_compress(open(CSS_FILE).read()))
         self.html = ensure_unicode(open(HTML_FILE).read())
         self.parser = parser
         self.res = res
+
+        csss = glob.glob(os.path.join(LIB_PATH, 'static/*.css'))
+        css_string = []
+        for css in csss:
+            logger.info("Load {}".format(os.path.basename(css)))
+            css = ensure_unicode(css_compress(open(css).read()))
+            css = u'<style type="text/css">{}</style>'.format(css)
+            css_string.append(css)
+        self.css_string = u"\n".join(css_string)
+
+        jss = glob.glob(os.path.join(LIB_PATH, 'static/*.js'))
+        js_string = []
+        for js in jss:
+            # TODO: add js compress
+            logger.info("Load {}".format(os.path.basename(js)))
+            js = ensure_unicode(open(js).read())
+            js = u'<script type="text/javascript">{}</script>'.format(js)
+            js_string.append(js)
+        self.js_string = u"\n".join(js_string)
 
     def get_avatar_pair(self, username):
         """ return base64 string pair of two avatars"""
@@ -61,12 +80,13 @@ class HTMLRender(object):
 
     def render_msg(self, msg):
         """ render a message, return the html block"""
+        sender = 'you' if not msg.isSend else 'me'
         # TODO
         try:
             template = ensure_unicode(TEMPLATES[msg.type])
             if msg.type == TYPE_SPEAK:
                 audio_str, duration = self.get_voice_mp3(msg.imgPath)
-                return template.format(sender_label='you' if not msg.isSend else 'me',
+                return template.format(sender_label=sender,
                                        voice_duration=duration,
                                        voice_str=audio_str)
             elif msg.type == TYPE_IMG:
@@ -75,23 +95,29 @@ class HTMLRender(object):
                 bigimgpath = self.parser.imginfo.get(msg.msgSvrId)
 
                 bigimg, smallimg = self.res.get_img([imgpath, bigimgpath])
-                return template.format(sender_label='you' if not msg.isSend else 'me',
-                                       img_data=smallimg)
+                return template.format(sender_label=sender,
+                                       small_img=smallimg,
+                                       big_img=bigimg)
             else:
                 raise
         except:
             template = ensure_unicode(TEMPLATES[1])
-            return template.format(sender_label='you' if not msg.isSend else 'me',
+            return template.format(sender_label=sender,
                                    content=msg.msg_str())
 
     def render_msgs(self, msgs):
+        """ render msgs of the same friend"""
         talker_name = msgs[0].talker
+        logger.info(u"Rendering messages of {}".format(talker_name))
         avatars = self.get_avatar_pair(talker_name)
         blocks = [self.render_msg(m) for m in msgs]
-        return self.html.format(style=self.css, talker=msgs[0].talker_name,
-                               messages=u''.join(blocks),
-                               avatar_me=avatars[0],
-                               avatar_you=avatars[1])
+
+        return self.html.format(extra_css=self.css_string,
+                                extra_js=self.js_string,
+                                talker=msgs[0].talker_name,
+                                messages=u''.join(blocks),
+                                avatar_me=avatars[0],
+                                avatar_you=avatars[1])
 
 if __name__ == '__main__':
     r = HTMLRender()
