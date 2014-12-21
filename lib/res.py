e2d2f5166fc31547fa4f7d9e50682e15b7ebae24
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-12-21 23:54:19 +0800

    minor fixes..

--- lib/res.py
+++ lib/res.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 # File: res.py
-# Date: Sun Dec 21 21:22:44 2014 +0800
+# Date: Sun Dec 21 23:49:46 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 import glob
@@ -157,6 +157,7 @@ class Resource(object):
             candidates = [k for k in candidates if not re.match('.*_[0-9]+$', k)]
             # only one file is the gif in need, others are frames and cover
             assert len(candidates) == 1
+        print candidates, md5
         fname = candidates[0]
         return Resource.get_file_b64(fname), imghdr.what(fname)
 
