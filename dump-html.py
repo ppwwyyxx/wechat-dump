268544183d6a798dc7cfb4e935ab394f514d7702
Yuxin Wu <ppwwyyxxc@gmail.com>
2016-04-21 18:12:57 -0400

    update script with argparse

--- dump-html.py
+++ dump-html.py
@@ -5,37 +5,48 @@
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 import sys
-if len(sys.argv) != 6:
-    sys.exit("Usage: {0} <path to decoded_database.db> <path to avatar.index> <path to resource> <name> <output html>".format(sys.argv[0]))
+import argparse
 
 from common.textutil import ensure_unicode
 from wechat.parser import WeChatDBParser
 from wechat.res import Resource
 from wechat.render import HTMLRender
 
-db_file = sys.argv[1]
-avt_db = sys.argv[2]
-resource_dir = sys.argv[3]
-name = ensure_unicode(sys.argv[4])
-output_file = sys.argv[5]
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help='name of contact')
+    parser.add_argument('--output', help='output html file', default='output.html')
+    parser.add_argument('--db', default='decoded.db', help='path to decoded database')
+    parser.add_argument('--avt', default='avatar.index', help='path to avatar.index file')
+    parser.add_argument('--res', default='resource', help='reseource directory')
+    args = parser.parse_args()
+    return args
 
-parser = WeChatDBParser(db_file)
-res = Resource(resource_dir, avt_db)
+if __name__ == '__main__':
+    args = get_args()
 
-try:
-    msgs = parser.msgs_by_chat[name]
-except:
-    sys.stderr.write(u"Valid Contacts: {}\n".format(u'\n'.join(parser.msgs_by_chat.keys())))
-    sys.stderr.write(u"Couldn't find that contact {}.".format(name));
-    sys.exit(1)
+    name = ensure_unicode(args.name)
+    output_file = args.output
 
-render = HTMLRender(parser, res)
-htmls = render.render_msgs(msgs)
+    parser = WeChatDBParser(args.db)
+    res = Resource(args.res, args.avt)
 
-if len(htmls) == 1:
-    with open(output_file, 'w') as f:
-        print >> f, htmls[0].encode('utf-8')
-else:
-    for idx, html in enumerate(htmls):
-        with open(output_file + '.{}'.format(idx), 'w') as f:
-            print >> f, html.encode('utf-8')
+    if name and name in parser.msgs_by_chat:
+        msgs = parser.msgs_by_chat[name]
+    else:
+        sys.stderr.write(u"Valid Contacts: {}\n".format(u'\n'.join(parser.msgs_by_chat.keys())))
+        sys.stderr.write(u"Couldn't find that contact {}.".format(name));
+        sys.exit(1)
+    print "Number of Messages: ", len(msgs)
+    assert len(msgs) > 0
+
+    render = HTMLRender(parser, res)
+    htmls = render.render_msgs(msgs)
+
+    if len(htmls) == 1:
+        with open(output_file, 'w') as f:
+            print >> f, htmls[0].encode('utf-8')
+    else:
+        for idx, html in enumerate(htmls):
+            with open(output_file + '.{}'.format(idx), 'w') as f:
+                print >> f, html.encode('utf-8')
